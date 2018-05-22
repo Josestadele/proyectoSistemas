0dd1b7c6c686b1fa35093a33d9e2d225b1cb33c4
Franco <franco2662@gmail.com>
2018-05-21 23:54:41 -0400

    Añadido archivo main y libreria para cada menu

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include"front.h"
+
+
+void main(int argc, char const *argv[]) {
+  int opcion;
+  do{
+   printf("                    Bienvenido \n\n\n");
+   printf("1.Empezar el juego\n");
+   printf("2.Cargar archivo\n");
+   printf("3.Salir\n");
+  scanf("%d",&opcion);
+  if (opcion==3)
+    exit(0);
+
+  if(opcion==2)
+  exit(0);
+  
+   if (opcion==1)
+  datosMatriz();
+
+  if ((opcion!=1)&&(opcion!=2)) {
+   printf("Error opcion incorrecta intente nuevamente \n");
+  }
+ }while ((opcion!=1)&&(opcion!=2));
+
+
+}
