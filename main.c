55c8e81142ae29dde34a4dcf48ba1df48bd2bb27
Franco Marino <franco2662@gmail.com>
2018-05-28 16:26:51 -0400

    Metodos para la matriz

--- main.c
+++ main.c
@@ -2,15 +2,33 @@
 #include<stdlib.h>
 #include<string.h>
 
+void llenarMatriz(int filas, int columnas, int matriz[20][20]){
+	for ( int i = 0 ; i < filas; i++ ) {
+            for ( int j = 0 ; j < columnas ; j++ ) 
+                fscanf(archivo,"%d",&matriz[i][j]);
+				}
+
+}
+
+
+void imprimirMatriz(int filas, int columnas, int matriz[20][20]){
+	for ( int i = 0 ; i < filas ; i++ ) {
+            for ( int j= 0 ; j < columnas ; j++ ) {
+                printf("%d ",matriz[i][j]);
+            }
+            printf("\n");
+        }	
+
+}
+
 
 
 void main(int argc, char const *argv[]) {
-  FILE *archivo;
- 
-  archivo = fopen("prueba.txt","r");
+  
   int opcion;
   int filas;
   int columnas;
+  int matriz[20][20];
 
   do{
    printf("                    Bienvenido \n\n\n");
@@ -21,15 +39,22 @@ void main(int argc, char const *argv[]) {
   if (opcion==3)
     exit(0);
 
-  if(opcion==2)
+  if(opcion==2){
+FILE *archivo;
+ 
+  archivo = fopen("prueba.txt","r");
    fscanf(archivo,"%d\n%d",&filas,&columnas);
- printf("Filas : %d \n",&filas);
- printf("Columnas : %d \n",&columnas);
+ printf("Filas : %d \n",filas);
+ printf("Columnas : %d \n",columnas);
+llenarMatriz(filas,columnas,matriz[20][20]);
+imprimirMatriz(filas,columnas,matriz[20][20]);
 
 
+
+}
 
    if (opcion==1)
-  datosMatriz();
+  exit(0);
 
   if ((opcion!=1)&&(opcion!=2)) {
    printf("Error opcion incorrecta intente nuevamente \n");
