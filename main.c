04262c054be0205079a6fddfc0c0dcfbb21b3eb4
Franco Marino <franco2662@gmail.com>
2018-05-28 17:17:20 -0400

    Modificacion funciones de matriz

--- main.c
+++ main.c
@@ -2,18 +2,30 @@
 #include<stdlib.h>
 #include<string.h>
 
-void llenarMatriz(int filas, int columnas, int matriz[20][20]){
-	for ( int i = 0 ; i < filas; i++ ) {
-            for ( int j = 0 ; j < columnas ; j++ ) 
-                fscanf(archivo,"%d",&matriz[i][j]);
-				}
+void llenarMatriz(int *filas, int *columnas, int matriz[20][20]){
+	printf("Se va a llenar matriz");
+	FILE *archivo;
+	archivo = fopen("prueba.txt","r");
+	printf("Se abrio archivo");
+	fscanf(archivo,"%d\n%d",filas,columnas);
+	printf("Filas : %d \n",*filas);
+ 	printf("Columnas : %d \n",*columnas);
 
+	for ( int i = 0 ; i < *filas; i++ ) {
+		for ( int j = 0 ; j < *columnas ; j++ ){
+                	fscanf(archivo,"%d",&matriz[i][j]);
+	  	}
+	}
+	fclose(archivo);
 }
 
 
-void imprimirMatriz(int filas, int columnas, int matriz[20][20]){
-	for ( int i = 0 ; i < filas ; i++ ) {
-            for ( int j= 0 ; j < columnas ; j++ ) {
+void imprimirMatriz(int *filas, int *columnas, int matriz[20][20]){
+	printf("Filas : %d \n",*filas);
+ 	printf("Columnas : %d \n",*columnas);
+
+	for ( int i = 0 ; i < *filas ; i++ ) {
+            for ( int j= 0 ; j < *columnas ; j++ ) {
                 printf("%d ",matriz[i][j]);
             }
             printf("\n");
@@ -24,7 +36,6 @@ void imprimirMatriz(int filas, int columnas, int matriz[20][20]){
 
 
 void main(int argc, char const *argv[]) {
-  
   int opcion;
   int filas;
   int columnas;
@@ -40,14 +51,10 @@ void main(int argc, char const *argv[]) {
     exit(0);
 
   if(opcion==2){
-FILE *archivo;
- 
-  archivo = fopen("prueba.txt","r");
-   fscanf(archivo,"%d\n%d",&filas,&columnas);
- printf("Filas : %d \n",filas);
- printf("Columnas : %d \n",columnas);
-llenarMatriz(filas,columnas,matriz[20][20]);
-imprimirMatriz(filas,columnas,matriz[20][20]);
+  printf("Se eligio opcion 2");
+llenarMatriz(&filas,&columnas,matriz);
+printf("Se lleno matriz");
+imprimirMatriz(&filas,&columnas,matriz);
 
 
 
